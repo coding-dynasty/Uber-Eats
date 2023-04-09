f8bb9b37c8cd6da7704688791a98a833e17c0356
Coding_Dynasty <eljerrariali8@gmail.com>
2023-04-09 00:15:16 +0000

    some formatting

--- src/components/categories/Categories.js
+++ src/components/categories/Categories.js
@@ -4,24 +4,25 @@ import Category from "./sub-components/Category";
 import categories from "./categories.json";
 
 const Categories = () => {
-  console.log(categories.categories[0]);
   return (
     <View>
       <View style={styles.top}>
-        {categories.categories[0]?.top?.map((category) => (
+        {categories?.categories[0]?.top?.map((category, id) => (
           <Category
             category={category.category}
             pos={category.pos}
             uri={category.uri}
+            key={id}
           />
         ))}
       </View>
       <View style={styles.bottom}>
-        {categories.categories[1]?.bottom?.map((category) => (
+        {categories?.categories[1]?.bottom?.map((category, id) => (
           <Category
             category={category.category}
             pos={category.pos}
             uri={category.uri}
+            key={id}
           />
         ))}
       </View>
